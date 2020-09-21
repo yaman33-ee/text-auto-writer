467e03a8e9f35968cde31a1974b7e53fe73d6843
yaman33-ee <58622859+yaman33-ee@users.noreply.github.com>
2020-09-21 10:01:04 +0300

    added the project

--- script.js
+++ script.js
@@ -2,20 +2,30 @@ var text;
 
 var index=0;
 var font_size=20;
+
 function generratetext()
 { 
   word=text.slice(0,index)
   index++;
   font_size+=2;
-
+ 
   if(index>text.length)index=0;
 
   document.querySelector(".formatted-text").innerText=word;
   document.querySelector(".formatted-text").style.fontSize=`${font_size}px`;
+  
 }
 
 document.querySelector("#submit").addEventListener('click',(e)=>{
   
   text=document.querySelector("#text").value 
-  setInterval(generratetext,1000)
+  var timesRun = 0;
+var interval = setInterval(function(){
+    timesRun += 1;
+    if(timesRun === 50){
+        clearInterval(interval);
+    }
+    //do whatever here..
+    generratetext()
+}, 1000); 
 })

e5d3348ae51b2c5cc0613abd6d97cbc050c675f5
yaman33-ee <58622859+yaman33-ee@users.noreply.github.com>
2020-09-21 10:04:26 +0300

    added the project

--- script.js
+++ script.js
@@ -20,12 +20,14 @@ document.querySelector("#submit").addEventListener('click',(e)=>{
   
   text=document.querySelector("#text").value 
   var timesRun = 0;
-var interval = setInterval(function(){
-    timesRun += 1;
-    if(timesRun === 50){
-        clearInterval(interval);
-    }
-    //do whatever here..
-    generratetext()
+  var startTime = new Date().getTime();
+  var interval = setInterval(function(){
+      if(new Date().getTime() - startTime > 60000){
+          clearInterval(interval);
+          return;
+      }
+      //do whatever here..
+      generratetext()
+  }, 2000);  
 }, 1000); 
 })
